4889a72877de85e52b53e21ea5d3b3f778350a48
ngckhanh <108393519+ngckhanh@users.noreply.github.com>
2022-09-07 14:46:57 +0700

    Add files via upload

---
+++ header_footer/footer.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+    <link type="text/css" rel="stylesheet" href="header_footer.css">
+
+</head>
+<body>
+    <footer>
+        <ul class="bottomnav">
+            <li><a href="aboutus.html">About Us</a></li>
+            <li><a href="copyright.html">Copyright</a></li>
+            <li><a href="privacy.html">Privacy</a></li>
+            <li><a href="helplinks.html">Help links</a></li>
+        </ul>
+    </footer>
+</body>
+</html>
---
+++ header_footer/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+    <link type="text/css" rel="stylesheet" href="header_footer.css">
+
+</head>
+<body>
+    <header id="header-bar" class="header-bar">
+        <div class="logo">
+            <a href="index.html">
+                <img src="../img/Logo.png" alt="logo"/>
+            </a> 
+        </div>
+        <ul class="topnav">
+            <li><a href="index.php" class="active">Home</a></li>
+            <li><a href="add_product.php">Add Product</a></li>
+            <li><a href="view_product.php">View Products</a></li>
+            <li><a href="my_account.php">My Account</a></li>
+            <li><a href="login.php">Login</a></li>
+        </ul>
+    </header>
+</body>
+</html>
+
